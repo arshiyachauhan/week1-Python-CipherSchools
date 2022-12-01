8e31c1e3bf7a73140ee666acb6af9199e221053c
arshiyachauhan <119539567+arshiyachauhan@users.noreply.github.com>
2022-12-01 23:54:36 +0530

    Add files via upload

---
+++ class2-basics of python-CipherSchools.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+#this is a comment
+print("Arshiya Chauhan")
+a="a"
+print(type(a))
+print(isinstance(print,object))
+a=255
+print(a)
+print(id(a))
+b=200
+print(id(b))
+c="aaa"
+print(id(c))
+a=1.234445678999
+print(a+1)
---
+++ class4-take user input and type conversion-CipherSchools.py
@@ -0,0 +1,8 @@
+a=15
+print(str(a))
+print(float(a))
+print(int("1234"))
+a='A'
+print(isinstance(a,object))
+a=65
+print(isinstance(a,object))
